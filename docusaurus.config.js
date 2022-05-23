33e7862f7f9b271887a389486ba402436d1b0430
Alan <alandawi@gmail.com>
2022-05-23 09:46:29 -0300

    Update docusaurus config file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,8 +16,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
+  deploymentBranch: 'gh-pages', // Usually your GitHub org/user name.
   organizationName: 'mediamonks', // Usually your GitHub org/user name.
   projectName: 'display-advertising-docs', // Usually your repo name.
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
